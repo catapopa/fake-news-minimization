d6e3b7098e855258b3ff8acf5accc5bf169a935a
Catalina Popa <catalina.popa@fundmore.ai>
2025-06-14 14:03:44 +0300

    run batch

--- src/04_compare_blocking_strategies.py
+++ src/04_compare_blocking_strategies.py
@@ -4,6 +4,7 @@ import ndlib.models.ModelConfig as mc
 import matplotlib.pyplot as plt
 import random
 import pickle
+from base import greedy_blocking
 from genetic import genetic_blocking_strategy
 from helper import simulate_icm_spread
 from config import CONFIG
@@ -32,7 +33,6 @@ degree_nodes = [n for n, _ in degrees[:k]]
 G2.remove_nodes_from(degree_nodes)
 [result_degree,_] = simulate_icm_spread(G2)
 
-
 # Strategy 3: High-Betweenness Blocking
 G3 = G.copy()
 betweenness = nx.betweenness_centrality(G3)
@@ -40,17 +40,24 @@ betweenness_nodes = sorted(betweenness, key=betweenness.get, reverse=True)[:k]
 G3.remove_nodes_from(betweenness_nodes)
 [result_betweenness,_] = simulate_icm_spread(G3)
 
+# Strategy 4: Greedy Blocking
+G4 = G.copy()
+greedy_nodes = greedy_blocking(G4, k)
+G4.remove_nodes_from(greedy_nodes)
+[greedy,_] = simulate_icm_spread(G4)
 
-# G4 = G.copy()
-# greedy_nodes = genetic_blocking_strategy(G4, k, CONFIG["genetic_config"])
-# G4.remove_nodes_from(greedy_nodes)
-# [ga_results,_] = simulate_icm_spread(G4)
+# Strategy 5: Genetic Algorithm Blocking
+G5 = G.copy()
+ga_nodes = genetic_blocking_strategy(G5, k, CONFIG["genetic_config"])
+G5.remove_nodes_from(ga_nodes)
+[genetic,_] = simulate_icm_spread(G5)
 
 # Plot comparison
 plt.plot(result_random, label="Random", linestyle='--', marker='o')
 plt.plot(result_degree, label="High Degree", linestyle='-', marker='s')
 plt.plot(result_betweenness, label="High Betweenness", linestyle='-.', marker='^')
-# plt.plot(ga_results, label="Genetic", linestyle='-.', marker='^')
+plt.plot(greedy, label="Greedy", linestyle='-.', marker='^')
+plt.plot(genetic, label="Genetic", linestyle='-.', marker='^')
 plt.xlabel("Iteration")
 plt.ylabel("Infected Nodes")
 plt.title(f"Spread Reduction with {k} Blocked Nodes")
